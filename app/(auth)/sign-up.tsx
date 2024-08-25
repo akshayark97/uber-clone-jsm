2e63499cfc9049fb3047578254d9f12ebe679410
Akshay <akshay@Akshays-MacBook-Air.local>
2024-08-25 16:35:09 +0530

    added first screen with skip button and swiper

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,13 @@
+import { Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SingUp = () => {
+  return (
+    <SafeAreaView>
+      <Text>SingUp</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SingUp;
